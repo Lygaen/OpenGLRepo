e03e43eada1eb86e76fec7398f73ccbda7c6d8b1
Lygaen <smalahcayeux@gmail.com>
2022-01-31 19:24:25 +0100

    inital commit

---
+++ build/libs/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Code/C++/CPPCraft/libs/glm/glm
+# Build directory: C:/Code/C++/CPPCraft/build/libs/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main(int, char **)
+{
+    std::cout << "Hello, world!\n";
+}
