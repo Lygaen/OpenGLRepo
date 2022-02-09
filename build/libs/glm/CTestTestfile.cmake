57b9f26a7238416d0cffd7141358d2e660c04a9d
Lygaen <53528937+Lygaen@users.noreply.github.com>
2022-02-09 13:46:43 +0100

    Deleted Build dir
    That was not meant to be pushed...

--- build/libs/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/C++/CPPCraft/libs/glm
-# Build directory: C:/Code/C++/CPPCraft/build/libs/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
